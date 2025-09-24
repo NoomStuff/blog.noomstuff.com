4d253e99ff8da26d381876d315c3430f1895e2dd
NoomStuff <NoomStuffMail@gmail.com>
2025-09-24 23:42:42 +0200

    First prod version

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -1,14 +1,23 @@
-document.addEventListener("DOMContentLoaded", () =>
-{
+document.addEventListener("DOMContentLoaded", () => {
     const bar = document.createElement("div");
     bar.id = "progress-bar";
     document.body.appendChild(bar);
 
-    function updateProgress()
-    {
-        const scrollTop = window.scrollY;
+    const banner = document.getElementById("banner");
+    const postEnd = document.getElementById("post-end");
+
+    let startOffset = 0;
+    let endOffset = 0;
+
+    if (banner) startOffset = banner.offsetHeight;
+    if (postEnd) endOffset = postEnd.offsetHeight;
+
+
+
+    function updateProgress() {
+        const scrollDistance = window.scrollY;
         const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
-        const scrollPercentage = (scrollTop / documentHeight) * 110;
+        const scrollPercentage = Math.max(((scrollDistance - startOffset) / (documentHeight - startOffset - endOffset)), 0) * 100;
         bar.style.width = scrollPercentage + "%";
     }
 
